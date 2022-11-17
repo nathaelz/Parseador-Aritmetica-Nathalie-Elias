87b6e361fc14641eb940d6b68bc2f885431420b0
nathaelz <nathaliemarie2303@gmail.com>
2022-11-17 17:45:48 -0400

    first commit

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
+
+namespace ParseadorAritmetica
+{
+    internal class Aritmetica : aritmeticaBaseVisitor<int>
+    {
+        public override int VisitAritmetica([NotNull] aritmeticaParser.AritmeticaContext context)
+        {
+            Console.WriteLine($"Visitando aritmetica: {context.GetText()}");
+            return base.VisitAritmetica(context);
+        }
+
+        public override int VisitExpresion([NotNull] aritmeticaParser.ExpresionContext context)
+        {
+            Console.WriteLine($"Visitando expresion: {context.GetText()}");
+            return base.VisitExpresion(context);
+        }
+
+        public override int VisitFactor([NotNull] aritmeticaParser.FactorContext context)
+        {
+            Console.WriteLine($"Visitando factor: {context.GetText()}");
+            return base.VisitFactor(context);
+        }
+
+        public override int VisitTermino([NotNull] aritmeticaParser.TerminoContext context)
+        {
+            Console.WriteLine($"Visitando termino: {context.GetText()}");
+            return base.VisitTermino(context);
+        }
+    }
+}
+
